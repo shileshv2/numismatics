af48a6c70520073cad8ba416bfbc3665e34bb921
Divyav2 <Divya.Badrinath@v2solutions.com>
2020-02-27 17:59:18 +0530

    files changed ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,13 @@
-angular.module("myCoins",[]);
+angular.module("myCoins", ["ui.router"]);
+
+angular.module("myCoins").config(function($stateProvider) {
+  $stateProvider
+    .state("adminPage", {
+      url: "/admin-login",
+      templateUrl: "/app/admin-login.html"
+    })
+    .state("usrPage", {
+      url: "/user-page",
+      templateUrl: "/app/user-page.html"
+    });
+});
