6613272f84004097d497d322c06ef4ae0dda40c7
Divyav2 <Divya.Badrinath@v2solutions.com>
2020-03-03 18:37:09 +0530

    modified app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,10 +10,27 @@ angular.module("myCoins").config(function($stateProvider) {
       url: "/user-page",
       templateUrl: "/app/user-page.html"
     })
+    .state("adminHome",{
+      url:"/admin-home",
+      templateUrl:"/app/admin-home.html",
+    })
     .state("inputPage",{
       url:"/input-data",
-      templateUrl:"appinput-data.html",
-      controller: 'inpuController'
+      templateUrl:"/app/input-data.html",
     });
 });
 
+angular.module("myCoins").controller("inputCtrl",function($scope){
+  $scope.formData={
+    title:"Dollar",
+    desc:"Sample Dollar description",
+    id: '1'
+  };
+  $scope.showData=$scope.formData;
+})
+
+
+angular.module("myCoins").controller("uploadImg",function($scope)
+{
+  
+})
